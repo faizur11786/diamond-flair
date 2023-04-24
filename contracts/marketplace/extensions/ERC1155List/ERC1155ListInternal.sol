45fb0a53c8728ae9e99248ad928beb889d227064
faizur <faizur11786@gmail.com>
2023-04-24 17:05:54 +0400

    nft list fixed

--- contracts/marketplace/extensions/ERC1155List/ERC1155ListInternal.sol
+++ contracts/marketplace/extensions/ERC1155List/ERC1155ListInternal.sol
@@ -2,16 +2,18 @@
 
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721, IERC165} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-import "../../../access/ownable/OwnableInternal.sol";
-import "../../base/MarketplaceBaseInternal.sol";
 import {IERC1155ListInternal} from "./interfaces/IERC1155ListInternal.sol";
-// import {ISokosNFT} from "./interfaces/ISokosNFT.sol";
 import {ERC1155ListStorage} from "./storage/ERC1155ListStorage.sol";
+import {OwnableInternal} from "../../../access/ownable/OwnableInternal.sol";
 import {MarketplaceBaseInternal} from "../../base/MarketplaceBaseInternal.sol";
+
+// import {ISokosNFT} from "./interfaces/ISokosNFT.sol";
+
+// import {MarketplaceBaseInternal} from "../../base/MarketplaceBaseInternal.sol";
 
 abstract contract ERC1155ListInternal is
     OwnableInternal,
