7d1a80aebb91eee849aad00a0c77ae5820bc45de
faizur <faizur11786@gmail.com>
2023-05-09 20:10:27 +0400

    Some bugs fixed in erc1155 contract

--- contracts/Tokens/ERC1155/ERC1155.sol
+++ contracts/Tokens/ERC1155/ERC1155.sol
@@ -12,8 +12,7 @@ import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ER
 import {ERC2771Context} from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {RoyaltiesV2Impl, LibPart} from "./RoyaltiesV2/RoyaltiesV2.sol";
-
-import "@openzeppelin/contracts/utils/Counters.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
 // import "@openzeppelin/contracts/access/AccessControl.sol";
 
@@ -29,6 +28,7 @@ contract SokosERC1155 is
     using Counters for Counters.Counter;
     Counters.Counter public tokenCounter;
     bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+    bytes4 private constant _INTERFACE_ID_ROYALTIES = 0xcad96cca;
 
     // bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     // bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
@@ -84,17 +84,22 @@ contract SokosERC1155 is
         return (address(0), 0);
     }
 
+    function stringToBytes(
+        string memory _string
+    ) public pure returns (bytes memory) {
+        return bytes(_string);
+    }
+
     function mint(
         uint256 amount,
-        bytes memory data,
-        string memory tokenURI,
+        bytes memory tokenURI,
         uint104 _royaltyPercentage,
         address payable _royaltyReceiver
     ) public onlyOwner {
         uint256 _id = tokenCounter.current();
         tokenCounter.increment();
-        _mint(_msgSender(), _id, amount, data);
-        _setURI(_id, tokenURI);
+        _mint(_msgSender(), _id, amount, tokenURI);
+        _setURI(_id, string(tokenURI));
         if (_royaltyPercentage > 0) {
             setRoyalties(_id, _royaltyReceiver, _royaltyPercentage);
         }
@@ -106,6 +111,12 @@ contract SokosERC1155 is
         uint256[] memory amounts,
         bytes memory data
     ) public onlyOwner {
+        require(ids.length == amounts.length, "Invalid length");
+        for (uint i = 0; i < ids.length; i++) {
+            if (!exists(ids[i])) {
+                tokenCounter.increment();
+            }
+        }
         _mintBatch(to, ids, amounts, data);
     }
 
@@ -159,6 +170,7 @@ contract SokosERC1155 is
     ) public view virtual override(ERC1155, IERC165) returns (bool) {
         return
             interfaceId == _INTERFACE_ID_ERC2981 ||
+            interfaceId == _INTERFACE_ID_ROYALTIES ||
             super.supportsInterface(interfaceId);
     }
 
