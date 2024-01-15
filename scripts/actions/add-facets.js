b978e5d4b9d56461e93c8467e60f5430f53fa895
faizur <faizur11786@gmail.com>
2024-01-15 11:12:53 +0400

    facets updated

--- scripts/actions/add-facets.js
+++ scripts/actions/add-facets.js
@@ -6,33 +6,33 @@ const { getSelectors, FacetCutAction } = require("../libraries/diamond.js");
 async function erc20Token() {
     const accounts = await ethers.getSigners();
     const contractOwner = accounts[0];
-    const diamondAddress = "0xA69c8e5436e6e754caE03A64cc14d39c94a05Ec8";
+    const diamondAddress = "0x93f5424AC74f24fb50a4082539A588aD42Ec66f6";
 
     const FacetNames = [
-        {
-            name: "ERC2771Context",
-            address: "0xac68df9c24181EB3d1B4AF229369504Cb2943Aef",
-        },
-        {
-            name: "ERC2771ContextOwnable",
-            address: "0x43e24477247DF68304B7Ef3ee03CA21196A8F06a",
-        },
+        // {
+        //     name: "ERC2771Context",
+        //     address: "0xac68df9c24181EB3d1B4AF229369504Cb2943Aef",
+        // },
+        // {
+        //     name: "ERC2771ContextOwnable",
+        //     address: "0x43e24477247DF68304B7Ef3ee03CA21196A8F06a",
+        // },
         {
             name: "Marketplace",
-            address: "0x1A83eD4a9E8dc9430C60c1F8b838a5e766f94C49",
+            address: null,
         },
-        {
-            name: "MarketplaceBaseOwnable",
-            address: "0x90B853BA15beaFf09D0057767D4EB8D4C223A1E3",
-        },
-        {
-            name: "Factory",
-            address: "0x36694906D649dE9BD473501ED9E4AE9c0309A2bd",
-        },
-        {
-            name: "TransferERC2771",
-            address: "0x95B18C2Fe2982f73ed93725690F8c87835110c66",
-        },
+        // {
+        //     name: "MarketplaceBaseOwnable",
+        //     address: "0x90B853BA15beaFf09D0057767D4EB8D4C223A1E3",
+        // },
+        // {
+        //     name: "Factory",
+        //     address: "0x36694906D649dE9BD473501ED9E4AE9c0309A2bd",
+        // },
+        // {
+        //     name: "TransferERC2771",
+        //     address: "0x95B18C2Fe2982f73ed93725690F8c87835110c66",
+        // },
     ];
     const cut = [];
     for (const FacetName of FacetNames) {
@@ -52,14 +52,14 @@ async function erc20Token() {
         cut.push({
             facetAddress: facet.address,
             action: FacetCutAction.Add,
-            functionSelectors: getSelectors(facet),
-            // .get([
-            //     "createERC721Collection(string,string,string,address)",
-            //     // "createERC1155Collection(string,string,string,address)",
-            //     // "cancelListing(address,uint256)",
-            //     // "getListingId(address,address,uint256)",
-            //     // "createListing(address,uint256,uint256,uint256,uint256,uint256)",
-            // ]),
+            functionSelectors: getSelectors(facet).get([
+                "offer(address,uint256,uint256,uint256,uint256)",
+                // "createERC721Collection(string,string,string,address)",
+                // "createERC1155Collection(string,string,string,address)",
+                // "cancelListing(address,uint256)",
+                // "getListingId(address,address,uint256)",
+                // "createListing(address,uint256,uint256,uint256,uint256,uint256)",
+            ]),
         });
     }
     console.log("Cut", cut);
--- scripts/test.js
+++ scripts/test.js
@@ -8,35 +8,10 @@ async function deployDiamond() {
     const accounts = await ethers.getSigners();
     const contractOwner = accounts[0];
 
-    const diamondAddress = "0x529aca21B345c84fEFF18192b37d4B2e12793093";
+    const diamondAddress = "0xA69c8e5436e6e754caE03A64cc14d39c94a05Ec8";
     const diamond = await ethers.getContractAt("Marketplace", diamondAddress);
-    const token = await ethers.getContractAt(
-        "SokosERC721",
-        "0x3cEa35CAb8485e29D19672c3e23f65387C7606BC"
-    );
     let tx;
     let receipt;
-
-    // tx = await diamond.createListing(
-    //     "0xe8e66e1D94F5AB0E236BF721aD2C9676d433db91",
-    //     "0x3cEa35CAb8485e29D19672c3e23f65387C7606BC",
-    //     "1",
-    //     "1",
-    //     "1000000"
-    // );
-    // tx = await token.mint(
-    //     contractOwner.address,
-    //     "TTS",
-    //     "0",
-    //     ethers.constants.AddressZero
-    // );
-    // tx = await token.setApprovalForAll(diamondAddress, true);
-    // tx = await diamond.offer(
-    //     "0x5d0CdCAd2276a31A3F7E9445a4b36F682784E573",
-    //     "0",
-    //     "10000",
-    //     "1693302404"
-    // );
 
     // tx = await diamond.cancelOffer("1");
 
@@ -46,7 +21,7 @@ async function deployDiamond() {
     //     throw Error(`Failed: ${tx.hash}`);
     // }
     // console.log("Completed", receipt);
-    console.log("tx: ", await diamond.offerOf(contractOwner.address, "2"));
+    console.log("tx: ", await diamond.decimals());
     return;
 }
 
